49ae026d4a094a7ce046930bc3cc662ae4663eb4
ARAVIND-HOMEPC\User <a.venkatachalam@sierradigitalinc.com>
2020-10-13 14:38:28 +0530

    no message

---
+++ reactjsmod/build/precache-manifest.8aba85c38aa5002cbd6e41760a8fba33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623ec3f85bc3f99d6cd6e598d08ecfbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e61d8ad2121187ca6a53",
+    "url": "/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "e860a12a3d4e8b2a25fe",
+    "url": "/static/js/2.cce40e94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cce40e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61d8ad2121187ca6a53",
+    "url": "/static/js/main.9f7a6102.chunk.js"
+  },
+  {
+    "revision": "eb71d235de02a69c01da",
+    "url": "/static/js/runtime-main.38c7c474.js"
+  }
+]);
